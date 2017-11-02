d3beae18680d832de8bfb1dc989dd6e02ff8fd6d
karkro <karol.krolikowski@gmail.com>
2017-11-02 22:16:59 +0100

    SumTwoNumbers.java

---
+++ SumTwoNumbers.java
@@ -0,0 +1,10 @@
+/*
+ * Program to print the sum of two numbers.
+ */
+
+public class SumTwoNumbers {
+	public static void main(String[] args) {
+		System.out.println("The sum of 21 and 23 is equal " + (21 + 23));
+	}
+
+}
