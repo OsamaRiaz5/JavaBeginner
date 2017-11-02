e0a722ca1d8c575ed87748db94a20aa6a2a6e240
karkro <karol.krolikowski@gmail.com>
2017-11-02 22:13:27 +0100

    SplittingText.java

---
+++ SplittingText.java
@@ -0,0 +1,10 @@
+/*
+ * Program to print "Hello" on Screen and then print your name on a separate line.
+ */
+
+public class SplittingText {
+	public static void main(String[] args) {
+		System.out.println("Hello\nKarol.");
+	}
+
+}
