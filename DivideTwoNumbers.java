db7cee7b2e3c82e59cab6027edcf267a4d26a6f9
karkro <karol.krolikowski@gmail.com>
2017-11-02 22:11:33 +0100

    DivideTwoNumbers.java

---
+++ DivideTwoNumbers.java
@@ -0,0 +1,10 @@
+/*
+ * Program to divide two numbers and print on the screen the result
+ */
+
+public class DivideTwoNumbers {
+	public static void main(String[] args) {
+		System.out.println("The result dividing 53/6 is equal " + 53 / 6);
+	}
+
+}
