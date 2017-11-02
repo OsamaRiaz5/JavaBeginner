9ce103ba5b9aac0f01ba610f3606fd4f9c3f95fc
karkro <karol.krolikowski@gmail.com>
2017-11-02 21:43:21 +0100

    AmericanFlag.java

---
+++ AmericanFlag.java
@@ -0,0 +1,30 @@
+/*
+ * Program to print an American flag on the screen
+ */
+
+public class AmericanFlag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("* * * * * * ==============================");
+		System.out.println(" * * * * *  ==============================");
+		System.out.println("* * * * * * ==============================");
+		System.out.println(" * * * * *  ==============================");
+		System.out.println("* * * * * * ==============================");
+		System.out.println(" * * * * *  ==============================");
+		System.out.println("* * * * * * ==============================");
+		System.out.println(" * * * * *  ==============================");
+		System.out.println("* * * * * * ==============================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+		System.out.println("==========================================");
+	}
+
+}
