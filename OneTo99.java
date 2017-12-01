3666605cb9d4253fac131995c1cf45b36af0b0ef
Karol Królikowski <karkro@zsi.ujk.edu.pl>
2017-12-01 15:20:45 +0100

    OneTo99.java

---
+++ OneTo99.java
@@ -0,0 +1,12 @@
+/*
+ * Program to print the odd numbers from 1 to 99, one number per line
+ */
+
+public class OneTo99 {
+	public static void main(String[] args) {
+		for (int i = 1; i <100; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
