85a2d72f65ca12eec95bd078a1dc5265ce55bd6b
Karol Królikowski <karkro@zsi.ujk.edu.pl>
2017-12-01 15:03:17 +0100

    SystemTime.java

---
+++ SystemTime.java
@@ -0,0 +1,11 @@
+/*
+ * Program to display the system time
+ */
+
+public class SystemTime {
+	public static void main(String[] args) {
+		System.out.format("\nCurrent Date time: %tc%n", System.currentTimeMillis());
+		
+	}
+
+}
