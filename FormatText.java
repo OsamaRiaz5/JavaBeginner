c05ec05f3677061002b2792bd40ff9a227ccc039
karkro <karol.krolikowski@gmail.com>
2017-12-05 18:50:33 +0100

    FormatText.java

---
+++ FormatText.java
@@ -0,0 +1,13 @@
+/*
+ * Program to print the following text in a specific format
+ */
+public class FormatText {
+	public static void main(String[] args) {
+		System.out.println("\nTwinkle, twinkle, little star,");
+		System.out.println("\tHow I wonder what you are!");
+		System.out.println("\t\tUp above the world so high,");
+		System.out.println("\t\tLike a diamond in the sky.");
+		System.out.println("Twinkle, twinkle, little star,");
+		System.out.println("\tHow I wonder what you are");
+	}
+}
