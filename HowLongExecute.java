33e576a9127ec0b208d4743cc3ae8470a1328db7
karkro <karol.krolikowski@gmail.com>
2018-01-17 20:46:20 +0100

    HowLongExecute.java

---
+++ HowLongExecute.java
@@ -0,0 +1,20 @@
+/*
+ * Program to measure how long some code takes to execute in nanoseconds.
+ */
+
+import java.lang.*;
+
+public class HowLongExecute {
+	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+		
+		// Simple program
+		System.out.println("10 first natural numbers: ");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+		long estimatedTime = System.nanoTime() - startTime;
+		System.out.println("Estimated time (in nanoseconds) to get the first 10 natural number: " + estimatedTime);
+	}
+}
